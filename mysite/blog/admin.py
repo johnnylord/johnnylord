7dcbf7fc18216fea48f1b23a4d776a80673fdfba
johnnylord <johnnylord7410@gmail.com>
2019-02-27 16:14:32 +0800

    Register Post an Category model to admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from mptt.admin import DraggableMPTTAdmin
+
+from .models import Post
+
 # Register your models here.
+class CategoryAdmin(DraggableMPTTAdmin):
+    prepopulated_fields = { 'slug': ('name',) }
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = { 'slug': ('title',) }
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+
