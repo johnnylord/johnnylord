1d18452d2015e6e43174306245723a8f93487d6b
johnnylord <johnnylord7410@gmail.com>
2019-02-27 15:26:18 +0800

    Design essential fields of Post model in blog app

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    # DATABASE FIELD
+    author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    title = models.CharField(max_length=75)
+    slug = models.SlugField()
+    feature_image = models.ImageField(blank=True, null=True)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    draft = models.BooleanField(default=True)
+    create_date = models.DateTimeField(default=timezone.localtime)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
